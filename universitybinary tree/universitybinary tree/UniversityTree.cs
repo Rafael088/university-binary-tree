1261f4a4705990c4a77580a8c46d052afcd8bf62
Rafael088 <79232629+Rafael088@users.noreply.github.com>
2021-04-23 13:23:50 -0500

    university-binary-tree

---
+++ universitybinary tree/universitybinary tree/UniversityTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace universitybinary_tree
+{
+    class UniversityTree
+    {
+        public PositionNode Root;
+
+        public void CreatePosition(PositionNode parent, Position positionToCreate, String parentPositionName){
+            PositionNode newNode = new PositionNode();
+            newNode.Position = positionToCreate;
+            if (Root==null) {
+                Root = newNode;
+                return;
+            }
+            if(parent==null) {
+                return;
+            }
+            if (parent.Position.Name==parentPositionName) { 
+                if(parent.Left==null) {
+                    parent.Left = newNode;
+                    return;
+                }
+                parent.Right = newNode;
+                return;
+            }
+            CreatePosition(parent.Left, positionToCreate, parentPositionName);
+            CreatePosition(parent.Right, positionToCreate, parentPositionName);
+        }
+        public void PrintTree(PositionNode from)
+        {
+            if(from==null) {
+                return;
+            }
+            Console.WriteLine($"{from.Position.Name} : ${from.Position.Salary}");
+            PrintTree(from.Left);
+            PrintTree(from.Right);
+        }
+        public float AddSalary(PositionNode from)
+        {
+            if (from == null)
+            {
+                return 0;
+            }
+            return from.Position.Salary + AddSalary(from.Left) + AddSalary(from.Right);
+          
+        }
+    }
+}
